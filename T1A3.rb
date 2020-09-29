ef8f7b050aa0fe6386b92bad57bbfdf71ffba453
VinsonWang92 <vinsonwang92@gmail.com>
2020-09-29 16:25:29 +1000

    created new ruby folder for Budget module

--- T1A3.rb
+++ T1A3.rb
@@ -1,26 +1,21 @@
+require_relative 'create_budget'
+
 begin
 cmd = nil
-        print 'Enter your command (create budget, add income, find budget surplus, quit): '
-        cmd = gets.chomp.downcase.strip
-        
-        case cmd
-        when 'create budget'
-            puts "How much is your rent/mortgage/board"
-            rent = gets.chomp.to_i
-            puts "How much do you spend on groceries?"
-            groceries = gets.chomp.to_i
-            puts "How much do you spend eating out?"
-            eating_out = gets.chomp.to_i
-            total_spending = rent+groceries+eating_out
-            puts "Your total monthly spending is $#{total_spending} per month"
-        when 'add income'
-            puts 'What is your net monthly income?'
-            income = gets.chomp.to_i 
-            puts "your net monthly income is $#{income}"
-        when 'find budget surplus'
-            budget_surplus = income-total_spending
-            puts "your net monthly surplus is $#{budget_surplus}"
-        end
-    end until ['quit', 'q'].include? cmd 
+    print 'Enter your command (create budget, add income, find budget surplus, quit): '
+    cmd = gets.chomp.downcase.strip
+    
+    case cmd
+    when 'create budget'
+       Budget::create_budget
+    when 'add income'
+        puts 'What is your net monthly income?'
+        income = gets.chomp.to_i 
+        puts "your net monthly income is $#{income}"
+    when 'find budget surplus'
+        budget_surplus = income-$total_spending
+        puts "your net monthly surplus is $#{budget_surplus}"
+    end
+end until ['quit', 'q'].include? cmd 
 
 
---
+++ create_budget.rb
@@ -0,0 +1,13 @@
+module Budget
+    def create_budget
+        puts "How much is your rent/mortgage/board"
+        rent = gets.chomp.to_i
+        puts "How much do you spend on groceries?"
+        groceries = gets.chomp.to_i
+        puts "How much do you spend eating out?"
+        eating_out = gets.chomp.to_i
+        $total_spending = rent+groceries+eating_out
+        puts "Your total monthly spending is $#{$total_spending} per month"
+    end
+    module_function :create_budget 
+end
