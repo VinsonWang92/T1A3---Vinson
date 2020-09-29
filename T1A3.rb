3fbfdfbe483c3b340840693e514ae50c6fa22e22
VinsonWang92 <vinsonwang92@gmail.com>
2020-09-29 17:26:48 +1000

    added invest option to application

--- T1A3.rb
+++ T1A3.rb
@@ -2,7 +2,7 @@ require_relative 'create_budget'
 
 begin
 cmd = nil
-    print 'Enter your command (create budget, add income, find budget surplus, quit): '
+    print 'Enter your command (create budget, add income, find budget surplus, invest, quit): '
     cmd = gets.chomp.downcase.strip
     
     case cmd
@@ -15,6 +15,16 @@ cmd = nil
     when 'find budget surplus'
         budget_surplus = income-$total_spending
         puts "your net monthly surplus is $#{budget_surplus}"
+    when 'invest'
+        if budget_surplus > 0
+            puts "What annual returns are you expecting from your investments?"
+            yields = gets.chomp.to_f
+            puts "How long are you planning to invest for in years?"
+            investment_time = gets.chomp.to_i
+            savings = budget_surplus*12*(1+yields/100)**investment_time
+            puts "If you stick to your budget and invest all your money at #{yields}% you'll have $#{savings} after #{investment_time} years"
+        else puts "there's no budget surplus!"
+        end
     end
 end until ['quit', 'q'].include? cmd 
 
