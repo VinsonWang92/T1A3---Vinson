a659c4aa4b7d1b5ecfaa9da8212a12dd45bd1d76
VinsonWang92 <vinsonwang92@gmail.com>
2020-10-01 14:51:45 +1000

    expanded questions and categories for creating budget

--- create_budget.rb
+++ create_budget.rb
@@ -1,12 +1,24 @@
 module Budget
     def create_budget
-        puts "How much is your rent/mortgage/board"
+        puts "How much is your rent/mortgage/board per month"
         rent = gets.chomp.to_i
-        puts "How much do you spend on groceries?"
+        puts "How much do you spend on groceries per month?"
         groceries = gets.chomp.to_i
-        puts "How much do you spend eating out?"
+        puts "How much do you spend eating out per month?"
         eating_out = gets.chomp.to_i
-        total_spending = rent+groceries+eating_out
+        puts "How much do you spend on clothing on average per month?"
+        clothing = gets.chomp.to_i
+        puts "How much do you spend on medical and health per month including private health insurance?"
+        health = gets.chomp.to_i
+        puts "How much do you spend on recreation and entertainment on average per month"
+        entertainment = gets.chomp.to_i
+        puts "How much do you budget per month for holidays?"
+        holidays = gets.chomp.to_i
+        puts "How much are your loan repayments per month? (credit card, personal loans, car loans etc)"
+        loans = gets.chomp.to_i
+        puts "Please enter how much other miscellaneous spending you think you may have per month, if 0, put 0"
+        miscellaneous = gets.chomp.to_i
+        total_spending = rent+groceries+eating_out+clothing+health+entertainment+holidays+loans+miscellaneous
         puts "Your total monthly spending is $#{total_spending} per month"
         return total_spending
     end
