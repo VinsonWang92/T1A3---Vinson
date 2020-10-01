6448964067eee04ba73301508305fcf3a7b60271
VinsonWang92 <vinsonwang92@gmail.com>
2020-10-01 15:11:11 +1000

    added budget summary table to display after budget is created with prompt to confirm if budget is correct

--- create_budget.rb
+++ create_budget.rb
@@ -19,7 +19,27 @@ module Budget
         puts "Please enter how much other miscellaneous spending you think you may have per month, if 0, put 0"
         miscellaneous = gets.chomp.to_i
         total_spending = rent+groceries+eating_out+clothing+health+entertainment+holidays+loans+miscellaneous
-        puts "Your total monthly spending is $#{total_spending} per month"
+        rows = []
+        rows << ["Rent", rent]
+        rows << ["Grocercies", groceries]
+        rows << ["Eating Out", eating_out]
+        rows << ["Clothing", clothing]
+        rows << ["Health", health]
+        rows << ["Entertainment", entertainment]
+        rows << ["Holidays", holidays]
+        rows << ["Loan repayments", loans]
+        rows << ["Miscellaneous", miscellaneous]
+        rows << ["Total Monthly Spending", total_spending]
+        budget_summary = Terminal::Table.new :title => "Budget Summary", :headings => ["Category", "Monthly Expense($)"], :rows=> rows
+        puts budget_summary
+        confirm = TTY::Prompt.new
+        confirm = confirm.yes?("Is this correct")
+        puts confirm
+        if confirm
+            puts "Move onto adding income"
+        else
+            puts "Please recreate budget with correct figures"
+        end
         return total_spending
     end
     module_function :create_budget 
