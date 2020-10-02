92c0ab52fa8834b99661b57e7f1aef8561822437
VinsonWang92 <vinsonwang92@gmail.com>
2020-10-02 17:11:08 +1000

    added conditions for income and budget to be created and surplus confirmed prior to invest option

--- T1A3.rb
+++ T1A3.rb
@@ -29,54 +29,60 @@ begin
     when 'Add Income'
         income = Income::add_income
     when 'Budget Surplus'
-        budget_surplus = income-total_spending
-        rows = []
-        rows << ["Budget Surplus", budget_surplus]
-        surplus_summary = Terminal::Table.new :title => "Net monthly savings", :rows=> rows
-        puts surplus_summary
-        if budget_surplus < 0 
-            puts "Income has to increase or spending has to decrease, please amend income or budget"
+        if total_spending && income !=nil
+            budget_surplus = income-total_spending
+            rows = []
+            rows << ["Budget Surplus", budget_surplus]
+            surplus_summary = Terminal::Table.new :title => "Net monthly savings", :rows=> rows
+            puts surplus_summary
+            if budget_surplus < 0 
+                puts "Income has to increase or spending has to decrease, please amend income or budget"
+            end
+        else 
+            puts "Please complete a budget and add income first"
         end
     when 'Invest'
-        if budget_surplus > 0
-            rows = []
-            rows << ["Shares", "10% p.a."]
-            rows << ["REIT (Real Estate Investment Trusts (property)", "7.8% p.a."]
-            rows << ["Term deposits", "4.2% p.a."]
-            rows << ["Bonds", "6.1% p.a."]
-            invest_summary = Terminal::Table.new :title => "Types of investments", :headings => ["Type of Investment", "Average returns over last 15 years"], :rows=> rows
-            puts invest_summary
-            yields = TTY::Prompt.new
-            yields = yields.select("What would you like to invest in?", ["Shares", "REIT", "Term_deposits", "Bonds"])
-            case yields
-                when "Shares"
-                    yields = 10
-                when "REIT" 
-                    yields = 7.8
-                when "Term_deposits" 
-                    yields = 4.2
-                when "Bonds"
-                    yields = 6.1
+        if budget_surplus != nil
+            if budget_surplus > 0
+                rows = []
+                rows << ["Shares", "10% p.a."]
+                rows << ["REIT (Real Estate Investment Trusts (property)", "7.8% p.a."]
+                rows << ["Term deposits", "4.2% p.a."]
+                rows << ["Bonds", "6.1% p.a."]
+                invest_summary = Terminal::Table.new :title => "Types of investments", :headings => ["Type of Investment", "Average returns over last 15 years"], :rows=> rows
+                puts invest_summary
+                yields = TTY::Prompt.new
+                yields = yields.select("What would you like to invest in?", ["Shares", "REIT", "Term_deposits", "Bonds"])
+                case yields
+                    when "Shares"
+                        yields = 10
+                    when "REIT" 
+                        yields = 7.8
+                    when "Term_deposits" 
+                        yields = 4.2
+                    when "Bonds"
+                        yields = 6.1
+                end
+                puts "How long are you planning to invest for in years?"
+                investment_time = gets.chomp.to_i
+                savings = (budget_surplus)*((1+yields/12/100)**(investment_time*12)/(yields/12/100))
+                rows = []
+                rows << [budget_surplus, investment_time, yields, savings]
+                savings_summary = Terminal::Table.new :title => "Net worth/savings summary", :headings => ["Savings contribution/month", "Investment Time(years)", "Investment yields(%)", "Total investment portfolio worth"], :rows=> rows
+                puts savings_summary
+                puts "If you stick to your budget and invest all your money at #{yields}% p.a. you'll have $#{savings} after #{investment_time} years"
+                chart = TTY::Prompt.new
+                chart = chart.yes?("Would you like to see a chart of your savings over #{investment_time} years?")
+                if chart
+                    puts AsciiCharts::Cartesian.new((0..investment_time).to_a.map{|x| [x, (budget_surplus*12)*((1+yields)**x/(yields))]}, :title => 'savings graph').draw
+                end
+            else
+                 puts "there's no budget surplus! Please create a another budget and add income to create a budget surplus"
             end
-            puts "How long are you planning to invest for in years?"
-            investment_time = gets.chomp.to_i
-            savings = (budget_surplus)*((1+yields/12/100)**(investment_time*12)/(yields/12/100))
-            rows = []
-            rows << [budget_surplus, investment_time, yields, savings]
-            savings_summary = Terminal::Table.new :title => "Net worth/savings summary", :headings => ["Savings contribution/month", "Investment Time(years)", "Investment yields(%)", "Total investment portfolio worth"], :rows=> rows
-            puts savings_summary
-            puts "If you stick to your budget and invest all your money at #{yields}% p.a. you'll have $#{savings} after #{investment_time} years"
-            chart = TTY::Prompt.new
-            chart = chart.yes?("Would you like to see a chart of your savings over #{investment_time} years?")
-            if chart
-                puts AsciiCharts::Cartesian.new((0..investment_time).to_a.map{|x| [x, (budget_surplus*12)*((1+yields)**x/(yields))]}, :title => 'savings graph').draw
-            end
-        else puts "there's no budget surplus! Please create a another budget and add income to create a budget surplus"
+        else 
+            puts "Please make sure you have completed a budget, added income and checked your budget surplus first"    
         end
-    when 'Save Profile'
-       
-
-    end  
+    end
 end until ['Quit', 'q'].include? cmd 
 
 
